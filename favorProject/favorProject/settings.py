287b4b0b5fca9906a3eb14ae208099e9eba2635b
Rupal Totale <rupaltotale>
2020-02-23 17:00:34 -0800

    remove verbosity

--- favorProject/favorProject/settings.py
+++ favorProject/favorProject/settings.py
@@ -50,7 +50,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=favorApp'
+    '--cover-package=favorApp',
 ]
 
 BOOTSTRAP4 = {
