87bd49a76d4489fb757faa57496014fdbf4f811a
Rupal Totale <rupaltotale@gmail.com>
2020-03-08 19:10:47 -0700

    tests fixed

--- favorProject/favorApp/models.py
+++ favorProject/favorApp/models.py
@@ -13,9 +13,6 @@ class Favor(models.Model):
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, blank=True, null=True)
     pendingUsers = models.ManyToManyField(User, related_name='pending_users')
-    # volunteer_event = models.BooleanField(default=False)
-    # requester_signed = models.BooleanField(default=False)
-    # giver_signed = models.BooleanField(default=False)
 
     def __str__(self):
         return "Title: {}, Description: {}, Owner: {}".format(self.title, self.description, self.owner)
